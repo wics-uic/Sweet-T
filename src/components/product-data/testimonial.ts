701758867841303ca096a25e9777f26055f3400c
cronusgk <dizondaniel163@yahoo.com>
2025-10-14 16:15:26 -0500

    Added random testimonial feature

---
+++ src/components/product-data/testimonial.ts
@@ -0,0 +1,23 @@
+import fs from 'fs'
+const filePath: string = './src/data/testimonials.json'
+
+const testJson = fs.readFileSync(filePath, 'utf-8');
+
+export type Testimonial = {
+    name: string;
+    message: string;
+};
+
+interface TestimonialsList {
+    [key: string]: Testimonial;
+}
+
+const data: TestimonialsList = JSON.parse(testJson)
+
+const testimonials: Testimonial[] = [];
+for (const key in data){
+    let test:Testimonial = data[key]
+    testimonials.push(test)
+}
+
+export default testimonials
