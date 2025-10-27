bce059daeab2cb9dd6d1bf7400f0254dd14dc593
zpate6 <zpate6@uic.edu>
2025-10-27 00:31:15 -0500

    added 24 cookie for user and decaled a userid for that cookie session

--- backend/Model/exampleModel.js
+++ backend/Model/exampleModel.js
@@ -1,1 +1,13 @@
-// This is am empty file
+import mongoose from "mongoose";
+
+const exampleSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        index: true
+    }
+}, {
+    collection: 'Examples'
+});
+
+export const Example = mongoose.model('Example', exampleSchema);
