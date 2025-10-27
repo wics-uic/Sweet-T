a23c138cec0d90f06af5f1caa129615db00b151e
cheetodustflori <florianneche@gmail.com>
2025-10-27 10:38:57 -0500

    zeel's changes

--- backend/Route/exampleRoute.js
+++ backend/Route/exampleRoute.js
@@ -1,1 +1,9 @@
-// This is am empty file
+import express from 'express';
+import { createExample } from '../Controller/exampleController.js';
+
+const router = express.Router();
+
+// POST - Create new example
+router.post('/createExample', createExample);
+
+export default router;
