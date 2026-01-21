be3478b5ff7d1dc9d2581db8757e3e7f702c4732
cheetodustflori <florianneche@gmail.com>
2026-01-20 23:18:37 -0600

    fixing astro config and package json and yaml

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://wics-uic.github.io',
-  base: '/Sweet-T',
-  output: 'server',
+  base: '/Sweet-T/',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: cloudflare()
+  }
+  // DO NOT include an 'adapter' property here
 });
